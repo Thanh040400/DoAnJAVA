a23477be0240fc0eb2deef479a4c55db8025cf52
Your Name <you@example.com>
2022-05-08 17:42:39 +0700

    done frmlogin

--- DoAn_java/src/view/formLogin.java
+++ DoAn_java/src/view/formLogin.java
@@ -8,6 +8,7 @@ import doan_java.ConnecToDatabase;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 /**
@@ -41,6 +42,8 @@ public class formLogin extends javax.swing.JFrame {
         jSeparator2 = new javax.swing.JSeparator();
         btnLogin = new javax.swing.JButton();
         btnReset = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        cbRoloe = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -73,6 +76,17 @@ public class formLogin extends javax.swing.JFrame {
             }
         });
 
+        jLabel4.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        jLabel4.setText("Quyền:");
+
+        cbRoloe.setFont(new java.awt.Font("Arial", 0, 20)); // NOI18N
+        cbRoloe.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Chọn quyền của bạn", "Quản Lý", "Nhân Viên" }));
+        cbRoloe.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbRoloeActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -80,29 +94,31 @@ public class formLogin extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jSeparator2, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jSeparator1)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(48, 48, 48)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
-                            .addComponent(jLabel2))
-                        .addGap(79, 79, 79)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel4))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txtUserName, javax.swing.GroupLayout.DEFAULT_SIZE, 292, Short.MAX_VALUE)
-                            .addComponent(txtPassword))
+                            .addComponent(txtUserName, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                            .addComponent(txtPassword)
+                            .addComponent(cbRoloe, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(btnLogin)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(btnReset)
-                        .addGap(113, 113, 113)))
+                    .addComponent(jSeparator2, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jSeparator1))
                 .addContainerGap())
             .addGroup(layout.createSequentialGroup()
                 .addGap(220, 220, 220)
                 .addComponent(jLabel1)
-                .addContainerGap(221, Short.MAX_VALUE))
+                .addContainerGap(235, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnLogin)
+                .addGap(18, 18, 18)
+                .addComponent(btnReset)
+                .addGap(114, 114, 114))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -120,40 +136,88 @@ public class formLogin extends javax.swing.JFrame {
                     .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnReset, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(51, Short.MAX_VALUE))
+                    .addComponent(cbRoloe, javax.swing.GroupLayout.DEFAULT_SIZE, 45, Short.MAX_VALUE)
+                    .addComponent(jLabel4))
+                .addGap(22, 22, 22)
+                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnReset, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(67, 67, 67))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
         ConnecToDatabase cn = new ConnecToDatabase();
         Connection conn = null;
-        try {
-            conn = cn.KetNoiCSDL();
-            String sql = "Select * From NHANVIEN Where TaiKhoan=? and MatKhau=?";
-            PreparedStatement pst = conn.prepareCall(sql);
-            pst.setString(1, txtUserName.getText());
-            pst.setString(2, txtPassword.getText());
-            ResultSet rs = pst.executeQuery();
-
-            if (rs.next()) {
-                JOptionPane.showMessageDialog(this,"Đăng nhập thành công");
-                frmThongKe Main = new frmThongKe();
-                Main.show();
-                this.hide();
-
+        while(true){
+            if(txtUserName.getText().trim().equals("")){
+                JOptionPane.showMessageDialog(null, "Tên tài khoản không được để trống.");
+                txtUserName.grabFocus();
+                return;
+            } else{
+                break;
             }
-            else{
-                JOptionPane.showMessageDialog(this,"Đăng nhập thất bại");
+        }
+        while(true){
+            if(txtPassword.getText().trim().equals("")){
+                JOptionPane.showMessageDialog(null, "Mật khẩu không được để trống.");
+                txtPassword.grabFocus();
+                return;
+            } else{
+                break;
             }
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(this,ex.toString());
+        }
+        if (cbRoloe.getSelectedIndex() == 2) {
+            try {
+               conn = cn.KetNoiCSDL();
+                String user = "Select * From NHANVIEN Where TaiKhoan=? and MatKhau=? and MaChucVu=?";
+                PreparedStatement pst;
+                pst = conn.prepareStatement(user);
+                pst.setString(1, txtUserName.getText());
+                pst.setString(2, txtPassword.getText());
+                pst.setInt(3, cbRoloe.getSelectedIndex());
+                ResultSet rs = pst.executeQuery();
+                if (rs.next()) {
+                     JOptionPane.showMessageDialog(this,"Đăng nhập thành công");
+                    formUser Main = new formUser();
+                    Main.show();
+                    this.hide();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Lỗi:: Sai tên tài khoản hoặc mật khẩu hoặc quyền không đúng.");
+                    txtUserName.grabFocus();
+                }
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Lỗi:: Không thể kết nối đến máy chủ");
+            }
+        }
+        if (cbRoloe.getSelectedIndex() == 1) {
+            try {
+               conn = cn.KetNoiCSDL();
+                String admin = "Select * From NHANVIEN Where TaiKhoan=? and MatKhau=? and MaChucVu=?";
+                PreparedStatement pst;
+                pst = conn.prepareStatement(admin);
+                pst.setString(1, txtUserName.getText());
+                pst.setString(2, txtPassword.getText());
+                pst.setInt(3, cbRoloe.getSelectedIndex());
+                ResultSet rs = pst.executeQuery();
+                if (rs.next()) {
+                     JOptionPane.showMessageDialog(this,"Đăng nhập thành công");
+                    frmMain Main = new frmMain();
+                    Main.show();
+                    this.hide();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Lỗi:: Sai tên tài khoản hoặc mật khẩu hoặc quyền không đúng.");
+                    txtUserName.grabFocus();
+                }
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Lỗi:: Không thể kết nối đến máy chủ");
+            }
         }
 
     }//GEN-LAST:event_btnLoginActionPerformed
@@ -162,6 +226,10 @@ public class formLogin extends javax.swing.JFrame {
         txtUserName.setText("");
         txtPassword.setText("");
     }//GEN-LAST:event_btnResetActionPerformed
+
+    private void cbRoloeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbRoloeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cbRoloeActionPerformed
 
     /**
      * @param args the command line arguments
@@ -201,9 +269,11 @@ public class formLogin extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnLogin;
     private javax.swing.JButton btnReset;
+    private javax.swing.JComboBox<String> cbRoloe;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JPasswordField txtPassword;
